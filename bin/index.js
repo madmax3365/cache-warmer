299dbe57bff43872b24e652b1b7d9bdbd3d3795f
root <root@rostrotuar.ru>
2020-08-16 03:11:53 +0200

    Fixes

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import crawl from '../lib/index.js';
+const crawl = require('../lib/index.js');
 
 const args = process.argv.splice(process.execArgv.length + 2);
 const site = `https://${args[0]}`;
